d2ae6499e4810e1ed6a650b9350bda2a64344a6d
Zoey Laytart <zlaytart@ucsc.edu>
2022-04-18 15:05:58 -0700

    added the background and colorful borders

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -4,6 +4,12 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(20, 20, "Rocket Patrol Play");
+        // green UI background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        // white borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
     }
 }

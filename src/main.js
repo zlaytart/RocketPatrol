41e0f7aec6c585d8af60283a3c7e3ffaa320fe3c
Zoey Laytart <zlaytart@ucsc.edu>
2022-04-18 16:12:15 -0700

    added movement to rocket

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ let config = {
     scene: [ Menu, Play ]
 }
 let game = new Phaser.Game(config);
+// reserve keyboard variables
+let keyF, keyR, keyLEFT, keyRIGHT;
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;

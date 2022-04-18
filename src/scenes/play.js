a2ec93facc5b7706ad98ed3dcffb4613659483a5
Zoey Laytart <zlaytart@ucsc.edu>
2022-04-18 15:30:47 -0700

    added rocket

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -20,6 +20,8 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        // add rocket (p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
     }
 
     update() {

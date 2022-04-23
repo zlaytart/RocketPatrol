efb97cc37c90857fcf90d4cd27279ea818af6def
Zoey Laytart <zlaytart@ucsc.edu>
2022-04-22 17:45:36 -0700

    added reset method

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -41,13 +41,13 @@ class Play extends Phaser.Scene {
         this.ship03.update();
         // check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)) {
-            console.log('kaboom ship 03');
+            this.p1Rocket.reset();
         }
         if(this.checkCollision(this.p1Rocket, this.ship02)) {
-            console.log('kaboom ship 02');
+            this.p1Rocket.reset();
         }
         if(this.checkCollision(this.p1Rocket, this.ship01)) {
-            console.log('kaboom ship 01');
+            this.p1Rocket.reset();
         }
     }
 

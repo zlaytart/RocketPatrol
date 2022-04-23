ef9d79cae6cc078cce5310bcbeac7189f10f649c
Zoey Laytart <zlaytart@ucsc.edu>
2022-04-22 20:02:56 -0700

    Menu screen

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -11,7 +11,26 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(20, 20, "Rocket Patrol Menu");
-        this.scene.start("playScene");
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        // show menu text
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, 'Use < > Arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+        menuConfig.backgroundColor = '#00FF00';
+        menuConfig.color = '#000000';
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press < for Novice or > for Expert', menuConfig).setOrigin(0.5);
+        // this.add.text(20, 20, "Rocket Patrol Menu");
+        // this.scene.start("playScene");
     }
 }
